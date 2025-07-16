f748f48b537dfccfd9f76236f64b172bbe281be6
Ingolf Wagner <contact@ingolf-wagner.de>
2025-07-17 01:20:49 +0200

    :building_construction: add options definitions

---
+++ modules/backend.nix
@@ -0,0 +1,76 @@
+{
+  lib,
+  ...
+}:
+with lib;
+with types;
+{
+
+  # folder structure
+  # ----------------
+  #
+  # $out/shared/<artifact-name>/<file-name>
+  # $out/machines/<machine-name>/<artifact-name>/<file-name>
+  #
+  # $input/shared/<artifact-name>/<file-name>
+  # $input/machines/<machine-name>/<artifact-name>/<file-name>
+  #
+  #
+  # deserialization:
+  # ----------------
+  # $input -> program -> $out
+  #
+  # serialization:
+  # --------------
+  # $out -> program
+
+  options = {
+    artifacts.backend = mkOption {
+      type = attrsOf (submodule {
+        options = {
+          serialize = mkOption {
+            type = package;
+            description = "script for serialization";
+          };
+
+          deserialize = mkOption {
+            type = package;
+            description = "script for deserialization";
+          };
+        };
+      });
+      default = { };
+      description = "Backend configurations for artifact serialization and deserialization";
+      example = literalExpression ''
+        artifacts.backend.default = config.artifacts.backend.passage;
+      '';
+    };
+  };
+
+  config = {
+
+    # predefined backends
+    artifacts.backend.passage = {
+      # will be called on each artifact
+      serialize = pkgs.writers.writeBash "serialize-with-passage" ''
+        export PATH=${pkgs.passage}:$PATH
+        for file in $(find "$out" -type f); do
+            # Remove the $out prefix to get the relative path
+            relative_path=''${file#$out/}
+            echo "Serialize: $relative_path"
+            cat "$file" | passage insert -m "artifacts/$relative_path"
+        done
+      '';
+      deserialize = pkgs.writers.writeBash "deserialize-with-passage" ''
+        export PATH=${pkgs.passage}:$PATH
+        for file in $(find "$input" -type f); do
+            # Remove the $input prefix to get the relative path
+            relative_path=''${file#$input/}
+            echo "Deserialize: $relative_path"
+            passage show  "artifacts/$relative_path" > $out/$relative_path
+        done
+      '';
+    };
+  };
+
+}
---
+++ modules/store.nix
@@ -0,0 +1,95 @@
+{ lib, config, ... }:
+with lib;
+with types;
+{
+  options.artifacts.store = mkOption {
+    type = attrsOf (
+      submodule (
+        { name, ... }:
+        let
+          artifactName = name;
+        in
+        {
+          options = {
+
+            shared = mkOption {
+              type = bool;
+              default = false;
+              description = "Whether the store is shared";
+            };
+
+            files = mkOption {
+              type = attrsOf (
+                submodule (
+                  { name, ... }:
+                  let
+                    fileName = name;
+                  in
+                  {
+                    options = {
+                      path = mkOption {
+                        type = str;
+                        default = "/run/secrets/${artifactName}/${fileName}";
+                        example = "/etc/ssh/ssh_host_ed25519_key";
+                        description = "Path to the file on the target system.";
+                      };
+
+                      owner = mkOption {
+                        type = str;
+                        default = "root";
+                        description = "owner of the file on the target system.";
+                      };
+
+                      group = mkOption {
+                        type = str;
+                        default = "root";
+                        description = "group of the file on the target system.";
+                      };
+                    };
+                  }
+                )
+              );
+              default = { };
+              description = "file definition on the target system";
+            };
+
+            prompts = mkOption {
+              type = attrsOf str;
+              default = { };
+              description = "Prompts end up in $prompt/<name> in the generator script";
+            };
+
+            serialize = mkOption {
+              type = package;
+              default = config.artifacts.backend.default.serialize;
+              description = "Serialization definition";
+            };
+
+            deserialize = mkOption {
+              type = package;
+              default = config.artifacts.backend.default.deserialize;
+              description = "Deserialization definition";
+            };
+
+            generator = mkOption {
+              type = nullOr package;
+              default = null;
+              description = ''
+                Generator Script. Two environment variables are handed over to this script.
+                - $prompt which is a folder containing files containing the prompt inputs (defined by the prompt option)
+                - $out which is a folder the generator script must create a file for each file definition of the artifact.
+              '';
+              example = literalExpression ''
+                pkgs.write.writeBash "random" ${"''"}
+                  ${pkgs.xkcdpass}/bin/xkcdpass --numwords 10 > $out/random_password
+                ${"''"};
+              '';
+            };
+          };
+        }
+      )
+    );
+    default = { };
+    description = "Artifacts store definitions";
+  };
+}
