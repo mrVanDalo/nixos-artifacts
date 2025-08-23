731180a248dc2261c0f9be1926d25e979147478c
Ingolf Wagner <contact@ingolf-wagner.de>
2025-08-23 13:50:27 +0200

    :tada: init artifacts-tui project

--- nix/devshells.nix
+++ nix/devshells.nix
@@ -1,8 +1,5 @@
 { inputs, ... }:
 {
-
-  imports = [ inputs.devshell.flakeModule ];
-
   perSystem =
     {
       pkgs,
@@ -11,26 +8,21 @@
       ...
     }:
     {
-
       # allow unfree packages
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
 
-      devshells.default = {
-
-        commands = [
-          {
-            help = "example command";
-            name = "example";
-            command = "echo 'this is an example command'";
-          }
-        ];
-
-        packages = [
-          # packages used in commands or in devshell
-        ];
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [
+            self'.formatter.outPath
+            pkgs.jetbrains.rust-rover
+            pkgs.rustup
+            pkgs.cargo-insta
+          ];
+        };
       };
     };
 }
